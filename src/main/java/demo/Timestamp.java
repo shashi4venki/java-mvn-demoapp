c2c897dd7af39f727efce6db22db8c9119e230d9
Venkat A <venkatakula@Venkats-MacBook-Air.local>
2021-10-25 11:18:02 -0700

    Formated timestamp

--- src/main/java/demo/Timestamp.java
+++ src/main/java/demo/Timestamp.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class Timestamp {
   public String getTimestamp() {
     LocalDateTime currentDateTime = LocalDateTime.now();
-    DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     String formattedDateTime = currentDateTime.format(formatter);
 		return formattedDateTime;
   }
